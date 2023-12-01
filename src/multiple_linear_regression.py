7933f9717ca98d7b4799c93ac3093b551a406e22
Yppilf2 <t.smeman@student.rug.nl>
2023-12-01 16:19:57 +0100

    outline

---
+++ src/multiple_linear_regression.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class MultipleLinearRegression:
+    def __init__(self):
+        """Initializes all required variables that are used throughout the class.
+        
+        Parameters:
+        
+        Returns:
+        """
+        pass
+    
+    def train(self, data):
+        """Trains the model using the multiple linear regression algorithm.
+
+        Parameters:
+        data - 2D array containing x and y values of the datapoints on which the algorithm is applied
+
+        Returns:
+        """
+        pass
+    
+    def predict(self, data):
+        """Makes a prediction of the incoming data, based on the data the model is trained on
+
+        Parameters:
+        data - 2D array based on which a prediction should be made
+
+        Returns:
+        """
+        pass
